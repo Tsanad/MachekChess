ec461a3e8727dd3f92fd85ee5b3a32d5a4e85867
Gyócsi Csanád <gy.csanad@gmail.com>
2022-09-16 14:58:17 +0200

    helo

---
+++ MachekChess/MachekChess.js
@@ -0,0 +1,13 @@
+class Piece{
+    constructor(type, color, position){
+        this.type = type
+        this.color = color
+        this.position = position
+    }
+}
+
+class Board{
+    constructor(){
+        startfen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+    }
+}
---
+++ MachekChess/Pieces/King.js
@@ -0,0 +1,11 @@
+class King{
+    constructor(pos, color){
+        this.pos = pos;
+        this.color = color;
+    }
+    move(postomove, board){
+        // TODO: isLegalmove()
+        this.pos=postomove
+
+    }
+}
